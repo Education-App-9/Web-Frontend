f8a25d94fba2994e819fc6b3d3198ca5512b057c
urooj-shaukat <uroojshaukat6@gmail.com>
2024-01-15 20:24:27 +0500

    login screen

--- src/Api/Auth/login.js
+++ src/Api/Auth/login.js
@@ -5,6 +5,6 @@ export const Login = async( email , password)=>{
         const response = await Axios.post('/auth/login' , {email , password} )
         console.log(response)
     } catch (error) {
-        console.log(error)
+        console.log(error.response.data.message)
     }
 }
